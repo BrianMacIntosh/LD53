64bbde1747be7fce4b06e00fb315341b8d9b35fe
Brian MacIntosh <brianamacintosh@gmail.com>
2023-04-29 10:43:16 -0700

    refrigerator

---
+++ Assets/Scripts/ToggleInteractable.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class ToggleInteractable : Interactable
+{
+	private Animator m_animator;
+
+	private bool m_toggleState = false;
+
+	private void Awake()
+	{
+		m_animator = GetComponent<Animator>();
+	}
+
+	public override void Interact(PlayerInteractor interactor)
+	{
+		m_toggleState = !m_toggleState;
+		m_animator.SetBool("ToggleState", m_toggleState);
+	}
+}
