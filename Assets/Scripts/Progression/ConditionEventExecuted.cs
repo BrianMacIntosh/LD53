ef87d54e2e96d36e65fccf9578f3331578d914b9
Brian MacIntosh <brianamacintosh@gmail.com>
2023-04-30 13:09:32 -0700

    add all orders to progression

---
+++ Assets/Scripts/Progression/ConditionEventExecuted.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(menuName = "Condition/Event Executed")]
+public class ConditionEventExecuted : BaseProgressionCondition
+{
+	[SerializeField]
+	private BaseProgressionEvent m_event;
+
+	public override bool Evaluate()
+	{
+		return ProgressionManager.Instance.HasEventExecuted(m_event);
+	}
+}
