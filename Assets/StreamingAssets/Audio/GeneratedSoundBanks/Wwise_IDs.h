45f150dbfad356052e6892eb647752de03be82b0
DESKTOP-C6356N6\Christian <thevoltwithin@outlook.com>
2023-04-30 17:25:16 -0400

    Added Voice_Over integration with sequence containers.

--- Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -16,11 +16,17 @@ namespace AK
         static const AkUniqueID DROP_ITEM = 1165005940U;
         static const AkUniqueID MUSIC = 3991942870U;
         static const AkUniqueID ROOM_RANDOM = 2482000886U;
+        static const AkUniqueID SEQUENCE1_VOICES = 88281251U;
+        static const AkUniqueID SEQUENCE2_VOICES = 2102032462U;
+        static const AkUniqueID SEQUENCE4_VOICES = 513774604U;
+        static const AkUniqueID VOICE_3_1 = 932918847U;
+        static const AkUniqueID VOICE_5_1 = 931933109U;
     } // namespace EVENTS
 
     namespace BANKS
     {
         static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID DIALOGUE = 3930136735U;
         static const AkUniqueID DROP_ITEM = 1165005940U;
         static const AkUniqueID MAIN_MUSIC = 63796330U;
         static const AkUniqueID ROOM_RANDOM = 2482000886U;
