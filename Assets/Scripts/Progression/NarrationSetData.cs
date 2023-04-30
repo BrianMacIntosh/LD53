d39a6d857291a792ada8a28de2c2e917e06cb743
Brian MacIntosh <brianamacintosh@gmail.com>
2023-04-29 18:49:35 -0700

    narration sets can play random lines

--- Assets/Scripts/Progression/NarrationSetData.cs
+++ Assets/Scripts/Progression/NarrationSetData.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using UnityEngine;
 
 [Serializable]
@@ -8,15 +9,29 @@ public struct NarrationLine
 	public string Caption;
 }
 
+public enum NarrationSetPlayMode
+{
+	Sequential,
+	RandomRepeat,
+	RandomNoRepeat,
+}
+
 [CreateAssetMenu(menuName = "Narration Set")]
 public class NarrationSetData : ScriptableObject
 {
 	public AK.Wwise.Event WwiseEvent;
 
+	public NarrationSetPlayMode PlayMode;
+
 	public NarrationLine[] Lines;
 
 	//HACK: storing data on this scriptable object, ick
 	private bool[] m_linesPlayed;
+
+	public int LineCount
+	{
+		get { return m_linesPlayed.Length; }
+	}
 
 	private void OnEnable()
 	{
@@ -35,5 +50,30 @@ public class NarrationSetData : ScriptableObject
 	public void MarkLinePlayed(int index)
 	{
 		m_linesPlayed[index] = true;
+	}
+
+	public int GetRandomUnplayedLine()
+	{
+		int unplayedLineCount = m_linesPlayed.Count(state => !state);
+		if (unplayedLineCount == 0)
+		{
+			return int.MaxValue;
+		}
+
+		int randomIndex = UnityEngine.Random.Range(0, unplayedLineCount);
+		for (int index = 0; index < Lines.Length; index++)
+		{
+			if (!m_linesPlayed[index])
+			{
+				if (randomIndex <= 0)
+				{
+					return index;
+				}
+				randomIndex--;
+			}
+		}
+
+		Debug.Assert(false);
+		return int.MaxValue;
 	}
 }
